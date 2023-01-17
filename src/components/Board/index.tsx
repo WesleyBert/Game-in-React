5abb75d069bc561f50a3067f87710830cba2922b
Your Name <wesleyberto4@gmail.com>
2023-01-17 18:23:48 -0300

    the hero config

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
 import Hero from "../Hero";
 import { GAME_SIZE } from "../../settings/constants";
+import MiniDemon from "..";
+import Demon from "../Demon";
+import Chest from "../Chest";
+import Trap from "../Trap";
 
 
 const Board = () => {
     return (
     <div>
         <Hero/>
+        <Demon/>
+        <MiniDemon/>
+        <Trap/>
+        <Chest/>
         <img src="./assets/tileset.gif" alt="" width={GAME_SIZE} height={GAME_SIZE}/>
     </div>
 
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,20 +1,48 @@
-import React from "react";
-import { TILE_SIZE } from "../../settings/constants";
+import useEventListener from "@use-it/event-listener";
+
+import React , {useState} from "react";
+import { TILE_SIZE, HEAD_OFFSET } from "../../settings/constants";
 
 import './index.css';
 
+const initialPosition = {
+    x: 15,
+    y: 15
+};
+
 const Hero = () => {
+    const [positionState, updatePositionState] = useState(initialPosition);
+    const [diretion, updateDiretionState] = useState('RIGHT');
+ 
+    useEventListener('keydown', (event:{key:any}) => {
+        if(event.key === 'ArrowLeft') {
+           updatePositionState({ x: positionState.x -1, y: positionState.y })
+           updateDiretionState('LEFT')
+        } else if (event.key === 'ArrowRinght'){
+               updatePositionState({x: positionState.x + 1, y: positionState.y,})
+               updateDiretionState('RIGHT')
+        } else if (event.key === 'ArrowDown'){
+               updatePositionState({ x: positionState.x, y: positionState.y - 1,  })
+        } else if (event.key === 'ArrowUp'){
+          updatePositionState({ x: positionState.x , y: positionState.y + 1}); 
+        }
+        
+    })
+
+
     return(
         <div 
         style={{
             position: 'absolute',
-            bottom: 48,
-            left: 0,
+            bottom: TILE_SIZE * positionState.y,
+            left: TILE_SIZE * positionState.x,
             width: TILE_SIZE,
-            height: 100,
+            height: TILE_SIZE + HEAD_OFFSET,
             backgroundImage: "url(./assets/HERO.png)",
             backgroundRepeat: "no-repeat",
-            animation: 'hero-animation 1s steps(4) infinite'
+            backgroundPosition: `0px -${ TILE_SIZE - HEAD_OFFSET}px`,
+            animation: 'hero-animation 1s steps(4) infinite',
+            transform: `scaleX(${diretion === 'RIGTH' ? 1 : -1})`
         }}
         />
     )
